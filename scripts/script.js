5674f9ee1814ffe26f4a31ee5f6e8e0de1f9f2f8
victorgarrido1 <vcgarr94@gmail.com>
2023-11-12 21:19:08 -0700

    Added radio buttons

--- scripts/script.js
+++ scripts/script.js
@@ -5,11 +5,12 @@ var QuestionCont = document.querySelector("#question-container");
 var TimerT = document.querySelector("#time");
 var QuestionIndex = 0;
 let numCorrect = 0;
+var QuestionForUser = document.querySelector("#question"); 
+var buttonA = document.getElementById("a_text");
+var buttonB = document.getElementById("b_text");
+var buttonC = document.getElementById("c_text");
+var buttonD = document.getElementById("d_text");
 
-var buttonA = document.getElementById("a");
-var buttonB = document.getElementById("b");
-var buttonC = document.getElementById("c");
-var buttonD = document.getElementById("d");
 
 
 
@@ -19,18 +20,15 @@ var timerID;
 let time = 75;
 
 function PopulateQuestion () {
+
+ 
+
     var QuestionTitle = myQuestions[QuestionIndex]
     var output = [];
 
     myQuestions.forEach(questions => output.push(questions));
-    innerHTML 
+     
 
-
-function buildQuiz() {
-    const output = [];
-
-    output.push({ question:"What does HTML mean?" })
-    return output
 }
 
 
@@ -47,7 +45,7 @@ function buildQuiz() {
 
     // Implement high scores page logic.
 
-}
+
 
 
 // start the quiz?
@@ -81,7 +79,58 @@ function endGame() {
     
 };
 
-startQuizButton.addEventListener("Click", startQuiz);
+
+ //Question Objects
+var myQuestions = [
+    {
+      question: "What does HTML mean?",
+      answers: {
+        a: "Hello Talk Meat Length",
+        b: "High Text Make Level",
+        c: "Hyper Text Markup Language",
+        d: "Hyper Tent Master Language",
+      },
+      correctAnswer: "c",
+    },
+  
+    {
+      question: "Explain what does hoisting mean?",
+      answers: {
+        a: "It means to bring the variables down to the bottom of the scope",
+        b: "A mechanism where variables are moved to the top of the scope",
+        c: "It means to bring the variables are moved to the middle of the scope",
+        d: "The variables are moved everywhere all in the scope",
+      },
+      correctAnswer: "b",
+    },
+  
+    {
+      question: "How do you view your inspector inside the browser?",
+      answers: {
+        a: "By doing a right click on your mouse and and selecting the inspector option.",
+        b: "By refreshing the web page and clicking the close button",
+        c: "Resetting the computer and going onto the Terminal on your computer",
+        d: "Going onto the Terminal on your computer and type Inspect",
+      },
+      correctAnswer: "a",
+    },
+  
+    {
+      question: "Name some font-related CSS attributes?",
+      answers: {
+        a: "Rubber duck, Large, Text Base, General",
+        b: "Font style, variant, weight",
+        c: "Big, Small, Tall, Little,",
+        d: "Enhance, Decrease, Clear, Zoom",
+      },
+      correctAnswer: "b",
+    },
+  ];
+
+  restart.addEventListener("click", () => {
+    initial();
+    displayContainer.classList.remove("hide")
+  });
 
 
 
